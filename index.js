483d72ce01a82bbaeffae1c12b1e1a6f679a935f
vytr5113-gif <vytr5113@gmail.com>
2026-04-27 14:35:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-console.log("STARTED");
+require("dotenv").config();
 
-setInterval(() => {}, 1000);
+const { Client, GatewayIntentBits } = require("discord.js");
+const http = require("http");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+// ВАЖНО — веб сервер для Render
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Bot is running");
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log("Web server running");
+});
+
+client.once("ready", () => {
+  console.log(`Bot logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
